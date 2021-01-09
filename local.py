14cb9728d33f80df981f203da31b1bbe1599afbd
hsiaofongw <hsiaofong.w@gmail.com>
2021-01-09 22:08:54 +0800

    update

--- local.py
+++ local.py
@@ -28,34 +28,16 @@ def start_on_local_mode(article_names, article_contents):
     doc_matrix = cut.make_doc_matrix(article_stats, all_term_vector)
 
     print('开始构建索引……')
-    article_indexes, term_indexes = cut.make_indexes(article_names, all_term_vector)
-
-    print('构建有效 term 索引的子集……')
-    selected_indexes = analysis.get_satisfied_indexes(term_indexes)
-
-    print('修正 term_indexes 与 doc_matrix ...')
-
-    print('修正前：')
-    print('doc_matrix.shape: ' + str(doc_matrix.shape))
-    print('term_indexes.shape: ' + str(term_indexes.shape))
-
-    term_indexes, doc_matrix = analysis.correct_doc_matrix(
-        term_indexes, 
-        selected_indexes, 
-        doc_matrix
+    index_to_article, term_to_index = cut.make_indexes(
+        article_names, 
+        all_term_vector
     )
-
-    print('修正后：')
-    print('doc_matrix.shape: ' + str(doc_matrix.shape))
-    print('term_indexes.shape: ' + str(term_indexes.shape))
 
     print('计算tf_idf ...')
     tf_idf = analysis.compute_tf_idf(doc_matrix)
 
     print('通过 SVD 方法进行 LSA 操作……')
-    u, s, vh, doc_coords = analysis.lsa_analysis(tf_idf)
-
-    term_to_col_index = analysis.make_term_to_col_index_mapper(term_indexes)
+    vh, doc_coords = analysis.lsa_analysis(tf_idf)
 
     print('开始REPL：')
     while True:
@@ -64,9 +46,8 @@ def start_on_local_mode(article_names, article_contents):
 
         query_result = analysis.do_query_by_words(
             words,
-            article_indexes,
             doc_coords,
-            vh, term_to_col_index
+            vh
         )
 
         print(tabulate(query_result, headers='keys', tablefmt='psql'))
